ec9ce500d50d179ae7607a5aa48fdddcffaa6a46
Sara Hu <shu012@ucr.edu>
2017-07-25 17:03:33 -0700

    Creates initial files

---
+++ UCR Student Toolbox/UCR Student ToolboxUITests/UCR_Student_ToolboxUITests.swift
@@ -0,0 +1,36 @@
+//
+//  UCR_Student_ToolboxUITests.swift
+//  UCR Student ToolboxUITests
+//
+//  Created by Sara Hu on 7/25/17.
+//  Copyright © 2017 Sara Hu. All rights reserved.
+//
+
+import XCTest
+
+class UCR_Student_ToolboxUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
