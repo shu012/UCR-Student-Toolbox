7c752a0f5d220134539b7928e9af4b4b8c1dabe8
Sara Hu <shu012@ucr.edu>
2017-07-30 18:57:04 -0700

    Adds map
    not centered yet or zoomed in

---
+++ UCR Student Toolbox/MapViewController.swift
@@ -0,0 +1,60 @@
+//
+//  MapViewController.swift
+//  UCR Student Toolbox
+//
+//  Created by Sara Hu on 7/30/17.
+//  Copyright © 2017 Sara Hu. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+//    let regionRadius: CLLocationDistance = 1000
+//    func centerMapOnLocation(location: CLLocation) {
+//        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+//                                                                  regionRadius * 2.0, regionRadius * 2.0)
+//        mapView.setRegion(coordinateRegion, animated: true)
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+//        let initialLocation = CLLocation(latitude: 33.9737, longitude: 117.3281)
+//        
+//        centerMapOnLocation(location: initialLocation)
+        
+        let location = CLLocationCoordinate2D(
+            latitude: 33.97533,
+            longitude: -117.329328
+        )
+        // 2
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        //3
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "University of California Riverside"
+        annotation.subtitle = "Riverside"
+        mapView.addAnnotation(annotation)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
